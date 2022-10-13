5f1897c7232010b57ec099cf2ec6f569e81498fb
root <1472942893@qq.com>
2022-10-13 18:04:21 +0800

    proxy pool

---
+++ settings.py
@@ -0,0 +1,20 @@
+import os
+from loguru import logger
+
+# 项目根目录
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+# redis
+REDIS_HOST = "192.168.1.233"
+REDIS_PORT = 6379
+REDIS_PASSWORD = "123456andy"
+REDIS_DB = 15
+
+
+
+
+
+
+
+
